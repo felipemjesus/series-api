46bc593c21026baea44cfea67fc52c7218ed92a7
Felipe Martins <felipemjesus@outlook.com>
2021-01-07 17:12:42 -0300

    Adicionado navegação entre recursos

--- app/Models/Episodio.php
+++ app/Models/Episodio.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Episodio extends Model
 {
     protected $fillable = ['temporada', 'numero', 'assistido', 'serie_id'];
+    protected $appends = ['links'];
 
     public function serie()
     {
@@ -16,5 +17,13 @@ class Episodio extends Model
     public function getAssistidoAttribute($assistido): bool
     {
         return $assistido;
+    }
+
+    public function getLinksAttribute(): array
+    {
+        return [
+            'self' => "/api/episodios/{$this->id}",
+            'serie' => "/api/series/{$this->serie_id}"
+        ];
     }
 }
--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -7,9 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Serie extends Model
 {
     protected $fillable = ['nome'];
+    protected $appends = ['links'];
 
     public function episodios()
     {
         return $this->hasMany(Episodio::class);
+    }
+
+    public function getLinksAttribute(): array
+    {
+        return [
+            'self' => "/api/series/{$this->id}",
+            'episodios' => "/api/series/{$this->id}/episodios"
+        ];
     }
 }
